754a9b57078be031ee0b803f56284c9ef3f078b6
lorenzo galli <lrnzgll@gmail.com>
2019-04-16 21:19:18 +0100

    many changes

--- app/facades/region/region_facade.rb
+++ app/facades/region/region_facade.rb
@@ -6,4 +6,12 @@ class Region::RegionFacade
   def counties
     @counties ||= County.where(region: @region).includes(:towns).order(name: :asc).group(:id).pluck(:name, :slug, Arel.sql('count(towns.id)'))
   end
+
+  def favourite_routes
+    @most_favourites_routes ||= @region.routes.order(cached_votes_total: :desc).limit(10).includes(:user).load
+  end
+
+  def new_routes
+    @new_routes ||= @region.routes.order(created_at: :desc).limit(10).includes(:user).load
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :regions
   resources :counties
   resources :routes
+  resources :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :home
